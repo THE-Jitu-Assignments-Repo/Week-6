9ecc6d12bbaedcd8f2a1d45b09aae5295ccfbe85
mik284 <mikeykamau222@gmail.com>
2022-11-13 20:20:49 +0300

    Creted week 6 projects and assignment task dir

---
+++ Test-Assignment3/Question-2.js
@@ -0,0 +1,26 @@
+// 2.	Name two hooks that help in optimizing performance of a react app explain how they help in improving
+//  performance. Write code to prove this.
+
+
+// - Two typical problems that cause poor performance in applications:
+
+//     1. Unnecessary re-renders.
+//     2 .Heavy computational calculation in the components repeatedly.
+
+
+•	useMemo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+•	useCallback
